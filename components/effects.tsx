4a74fbc65ca351ebc86749ce4d6f00a0790bb9c9
Satwikloka-dev <satwik@zithara.in>
2026-03-26 02:52:25 +0530

    build issue

--- components/effects.tsx
+++ components/effects.tsx
@@ -153,7 +153,7 @@ export function GlowEffect({
   const BASE_TRANSITION = {
     repeat: Number.POSITIVE_INFINITY,
     duration,
-    ease: "linear",
+    ease: "linear" as const,
   };
 
   const animations = {
