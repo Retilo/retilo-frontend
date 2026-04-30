2d4dd9871178ddaa0f71709499e2346440db9e0d
Satwikloka-dev <satwik@zithara.in>
2026-05-01 01:35:11 +0530

    idk

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,11 @@ const nextConfig: NextConfig = {
   transpilePackages: ["deck.gl", "@deck.gl/core", "@deck.gl/layers", "@deck.gl/mapbox", "@deck.gl/mesh-layers"],
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "eleven-public-cdn.elevenlabs.io",
-      },
+      { protocol: "https", hostname: "eleven-public-cdn.elevenlabs.io" },
+      { protocol: "https", hostname: "i.pravatar.cc" },
+      { protocol: "https", hostname: "logo.clearbit.com" },
+      { protocol: "https", hostname: "maps.googleapis.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
     ],
   },
 };
